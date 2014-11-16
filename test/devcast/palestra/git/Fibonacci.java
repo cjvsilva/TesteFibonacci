475dd546fb175c25285e8f74f27f01b8d1b4b4ec
Cicero Jeferson <jefersoncj@hotmail.com>
2014-11-16 03:49:32 -0300

    primeiro commit. Teste de Fibonacci de 0 e sua implementação

---
+++ test/devcast/palestra/git/Fibonacci.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package devcast.palestra.git;
+
+/**
+ *
+ * @author jeferson
+ */
+class Fibonacci {
+    
+    public int calcularFibonacci(int numero){
+        
+        
+        return 0;
+        
+    }
+}
